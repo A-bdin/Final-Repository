add3ce70871940782c5a36a4604f4025cad70fe7
ibrahiminyass-gif <ibrahiminyass@iut-dhaka.edu>
2026-03-27 21:11:11 +0600

    Update CreditCardPayment.java

--- src/CreditCardPayment.java
+++ src/CreditCardPayment.java
@@ -1,1 +1,13 @@
+class CreditCardPayment implements PaymentStrategy {
+    private String card;
+    private FakeBank bank;
 
+    public CreditCardPayment(String card, FakeBank bank) {
+        this.card = card;
+        this.bank = bank;
+    }
+
+    public boolean pay(double amount) {
+        return bank.pay(card, amount);
+    }
+}
